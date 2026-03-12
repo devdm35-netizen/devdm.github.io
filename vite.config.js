c205d9639d2c2d5bce6fe185093b218bd1e92674
Danilo yered De la cruz mendoza <daniloyereddelacruzmendoza@PAPELERIA-BLUETOOTH.local>
2026-03-12 14:41:12 -0500

    version 1.1.2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/acomodador-imagenes/", // 
+   base: "https:devdm35-netizen.github.io/devdm.github.io", // 
 })
